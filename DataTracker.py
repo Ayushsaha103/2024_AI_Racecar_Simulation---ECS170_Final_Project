bd403e073a99b903b3635075265c3a2040f690ba
keith-loww <keithlow1@hotmail.com>
2024-06-08 19:42:15 -0700

    fixes + ephi

--- DataTracker.py
+++ DataTracker.py
@@ -2,28 +2,43 @@ from stable_baselines3.common.callbacks import BaseCallback
 import pandas as pd
 import numpy as np
 
+
 class DataTracker(BaseCallback):
     def __init__(self, verbose=0):
         super(DataTracker, self).__init__(verbose)
-        self.data = pd.DataFrame(columns=['episode', 'avg_reward'])
+        self.data = pd.DataFrame(columns=["episode", "avg_reward", "avg_ephi"])
         self.episode_rewards = []
         self.current_rewards = []
+        self.current_ephis = []
 
     def _on_step(self) -> bool:
-        self.current_rewards.append(self.training_env.get_attr('reward')[0])
-        return True
+        self.current_rewards.append(self.training_env.get_attr("reward")[0])
 
-    def _on_rollout_end(self) -> bool:
-        self.episode_rewards.append(np.mean(self.current_rewards))
-        self.current_rewards = []
+        obs = self.training_env.get_attr("obs")[0]
+        self.current_ephis.append(obs.ephi)
 
-        new_row = pd.DataFrame([{'episode': len(self.episode_rewards), 'avg_reward': self.episode_rewards[-1]}])
-        self.data = pd.concat([self.data, new_row], ignore_index=True)
-        
+        if self.locals.get("done"):
+            self.episode_rewards.append(np.mean(self.current_rewards))
+            self.current_rewards = []
+
+            avg_ephi = np.mean(self.current_ephis)
+            self.current_ephis = []
+
+            new_row = pd.DataFrame(
+                [
+                    {
+                        "episode": len(self.episode_rewards),
+                        "avg_reward": self.episode_rewards[-1],
+                        "avg_ephi": avg_ephi,
+                    }
+                ]
+            )
+            self.data = pd.concat([self.data, new_row], ignore_index=True)
+
         return True
 
     def get_data(self):
         return self.data
-    
-    def save_data(self, filename, index=False):
+
+    def save(self, filename, index=False):
         self.data.to_csv(filename, index=index)
